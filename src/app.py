8bb52808614fdeb9409e2af00668926b4f7f5fb1
Duong <duonglequy.24.08.2003@gmail.com>
2026-02-02 15:44:57 +0700

    fix bug: error when run multiple process on window

--- src/app.py
+++ src/app.py
@@ -1,26 +1,27 @@
 import os
-os.environ["TOKENIZERS_PARALLELISH"] = "false"
+import uvicorn
+import multiprocessing
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+from dotenv import load_dotenv
+load_dotenv()
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from langserve import add_routes
 
 from src.base.llm_model import get_hf_llm, get_llama_llm
 from src.rag.main import build_rag_chain, InputQA, OutputQA
 
-# llm = get_hf_llm(temperature = 0.5)
-llm = get_llama_llm(model_file="./src/base/llama-2-7b-chat.Q6_K.gguf")
-genai_docs = "./data_source/generative_ai"
 
-genai_chain = build_rag_chain(llm, data_dir=genai_docs, data_type="pdf") # create chains
-
-# ------------- Create app FastAPI ---------------
+PATH_MODEL = os.environ.get("PATH_MODEL")
+GENAI_DOCS = os.environ.get("PATH_TO_DOCS")
 
 app = FastAPI(
-    title= "LangChain Server", 
-    version = "1.0",
-    description = "My first simple API server using LangChain"
+    title="LangChain Server",
+    version="1.0",
+    description="My first simple API server using LangChain"
 )
 
 app.add_middleware(
@@ -32,14 +33,42 @@ app.add_middleware(
     expose_headers=["*"]
 )
 
-# ------------- Add routes to app ---------------
+genai_chain = None
+
+def init_rag_chain():
+    # llm = get_hf_llm(temperature=0.5)
+    llm = get_llama_llm(model_file=PATH_MODEL)
+
+    return build_rag_chain(
+        llm,
+        data_dir=GENAI_DOCS,
+        data_type="pdf"
+    )
+
+
+# --------- Routes ----------
 @app.get("/check")
 async def check():
     return {"status": "OK"}
 
 @app.post("/generative_ai", response_model=OutputQA)
-async def generative_ao(input: InputQA):
+async def generative_ai(input: InputQA):
     answer = genai_chain.invoke(input.question)
     return {"answer": answer}
 
-add_routes(app, genai_chain, playground_type="default", path="/generative_ai") # langserve
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    multiprocessing.set_start_method("spawn", force=True)
+
+    genai_chain = init_rag_chain()
+
+    # LangServe routes (add only after chain ready)
+    add_routes(
+        app,
+        genai_chain,
+        playground_type="default",
+        path="/generative_ai"
+    )
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/base/llm_model.py
+++ src/base/llm_model.py
@@ -1,8 +1,8 @@
 import torch
 from transformers import BitsAndBytesConfig
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain_community.llms import CTransformers
+from langchain_community.llms import HuggingFacePipeline
 
 nf4_config = BitsAndBytesConfig(
     load_in_4bits = True,
